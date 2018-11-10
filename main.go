7beaecf3458b77624e1290fcaa296a36a68fed5a
Adrian Emil Chambe-Eng <adrianceng@gmail.com>
2018-11-10 19:43:25 +0100

    1.0, everything works, only a few minor improvements can be made here or there

--- main.go
+++ main.go
@@ -1,58 +1,196 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math"
 	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/marni/goigc"
 )
 
-func handlerMetaAPI(w http.ResponseWriter, r *http.Request) {
-	/*
-		~~GET~~
-		Return meta information about the API
-			"uptime": <uptime>, (formatted to ISO 8601)
-			"info": "Service for IGC tracks.",
-			"version": "v1",
-		response-type: application/json
-		response code: 200
-	*/
+//StartTime ...
+var StartTime time.Time
+
+//MetaInf ..
+var MetaInf []metaTrack
+
+//Tracks ...
+var Tracks []igc.Track
+
+type metaInf struct {
+	Uptime  string `json:"uptime"`
+	Info    string `json:"info"`
+	Version string `json:"version"`
 }
 
-func handlerTracks(w http.ResponseWriter, r *http.Request) {
-	/*
-		~~POST~~
-		register tracks, response-type: application/json, "handle all errors gracefully" ;)
-		request template:
-			"url": "<url>"  <- url is a normal url
-		response template:
-			"id": "<id>" <- unique id to that specific file upload
+type trackIn struct {
+	URL string `json:"url"`
+}
 
-		~~GET~~
-		return the array of all track ID's
-			[<id1>, <id2>, ...]
-		response-type: application/json
-		response code: 200, or appropriate error code
-		response: the array of ID's, or an empty array if no tracks have been stored yet
-	*/
+type trackOut struct {
+	ID int `json:"id"`
+}
+
+type metaTrack struct {
+	Hdate       time.Time `json:"H_date"`
+	Pilot       string    `json:"pilot"`
+	GliderType  string    `json:"glider"`
+	GliderID    string    `json:"glider_id"`
+	TrackLength float64   `json:"track_length"`
+}
+
+func durationFormat(sec float64) string {
+	var days, hours, minutes, seconds float64
+
+	if sec > 86400 {
+		seconds = math.Mod(sec, 86400.0)
+		days = math.Trunc(sec / 86400.0)
+		sec = seconds
+	}
+	if sec > 3600 {
+		seconds = math.Mod(sec, 3600.0)
+		hours = math.Trunc(sec / 3600.0)
+		sec = seconds
+	}
+	if sec > 60 {
+		seconds = math.Mod(sec, 60.0)
+		minutes = math.Trunc(sec / 60.0)
+		sec = seconds
+	}
+	upTime := "P" + strconv.FormatFloat(days, 'f', 0, 64) + "DT" + strconv.FormatFloat(hours, 'f', 0, 64) + "H" + strconv.FormatFloat(minutes, 'f', 0, 64) + "M" + strconv.FormatFloat(sec, 'f', 0, 64) + "S"
+	return upTime
+}
+
+func handlerAPI(w http.ResponseWriter, r *http.Request) {
+	d := time.Since(StartTime)
+	dur := d.Seconds()
+	upTime := durationFormat(dur)
+
+	metadata := metaInf{upTime, "Service app for IGC tracks", "v1"}
+	http.Header.Add(w.Header(), "content-type", "application/json")
+	json.NewEncoder(w).Encode(metadata)
+}
+
+func handlerTracksIn(w http.ResponseWriter, r *http.Request) {
+	dec := json.NewDecoder(r.Body)
+	var input trackIn
+
+	err := dec.Decode(&input)
+	if err != nil {
+		http.Error(w, "Bad Request", 400)
+		return
+	}
+	inurl, err := url.Parse(input.URL)
+	if err != nil {
+		http.Error(w, "Bad Request", 400)
+		return
+	}
+	igcsrc := inurl.String()
+	tempTrack, err := igc.ParseLocation(igcsrc)
+	if err != nil {
+		http.Error(w, "Bad Request", 400)
+		return
+	}
+	Tracks = append(Tracks, tempTrack)
+	id := len(Tracks)
+	updateMeta(id)
+
+	json.NewEncoder(w).Encode(id)
+}
+
+func handlerTracksOut(w http.ResponseWriter, r *http.Request) {
+	id := len(Tracks)
+
+	var idArray []int
+	for i := 1; i <= id; i++ {
+		idArray = append(idArray, i)
+	}
+
+	json.NewEncoder(w).Encode(idArray)
 }
 
 func handlerMetaTrack(w http.ResponseWriter, r *http.Request) {
-	/*
-		~~GET~~ /api/igc/<id>
-		return the meta information about a track with provided <id>, or NOT FOUND code
-		application/json
-		200 or appropriate
-		Response:
-			"H_date": 		<date from File Header, H-record>,
-			"pilot":		<pilot>,
-			"glider":		<glider>,
-			"glider_id":	<glider_id>,
-			"track_length":	<calculated total track length>
+	id, _ := findParams(r)
+	if id >= len(Tracks) {
+		http.Error(w, "Not Found", 404)
+		return
+	}
 
-		~~GET~~ /api/igc/<id>/<field>
-		return the single detailed meta information or NOT FOUND
-		response should always be a string, with the exception of the calculated total length
-	*/
+	json.NewEncoder(w).Encode(MetaInf[id])
+}
+
+func handlerSpecificTrack(w http.ResponseWriter, r *http.Request) {
+	id, field := findParams(r)
+
+	if id >= len(Tracks) {
+		http.Error(w, "Not Found", 404)
+		return
+	}
+
+	switch field {
+	case "pilot":
+		fmt.Fprintln(w, MetaInf[id-1].Pilot)
+	case "glider":
+		fmt.Fprintln(w, MetaInf[id-1].GliderType)
+	case "glider_id":
+		fmt.Fprintln(w, MetaInf[id-1].GliderID)
+	case "track_length":
+		fmt.Fprintln(w, MetaInf[id-1].TrackLength)
+	case "H_date":
+		fmt.Fprintln(w, MetaInf[id-1].Hdate)
+	default:
+		http.Error(w, "Bad Request", 400)
+		return
+	}
+}
+
+func updateMeta(id int) {
+	var tempMeta = metaTrack{
+		Pilot:       Tracks[id-1].Header.Pilot,
+		GliderType:  Tracks[id-1].Header.GliderType,
+		GliderID:    Tracks[id-1].Header.GliderID,
+		TrackLength: Tracks[id-1].Task.Distance(),
+		Hdate:       Tracks[id-1].Header.Date,
+	}
+	MetaInf = append(MetaInf, tempMeta)
+	fmt.Println(len(MetaInf), len(Tracks))
+}
+
+func findParams(r *http.Request) (int, string) {
+	in := strings.Trim(r.URL.Path, "/lgcinfo/api/igc/")
+	parts := strings.Split(in, "/")
+	if len(parts) == 1 {
+		id, err := strconv.Atoi(parts[0])
+		if err != nil {
+			//error
+			return 0, ""
+		}
+		return id, ""
+	}
+	id, err := strconv.Atoi(parts[0])
+	if err != nil {
+		//error
+		return 0, ""
+	}
+	field := parts[1]
+	return id, field
 }
 
 func main() {
-
+	StartTime = time.Now()
+	r := mux.NewRouter()
+	//port := os.Getenv("PORT")
+	r.HandleFunc("/igcinfo/api", handlerAPI).Methods("GET")
+	r.HandleFunc("/igcinfo/api/igc", handlerTracksIn).Methods("POST")
+	r.HandleFunc("/igcinfo/api/igc", handlerTracksOut).Methods("GET")
+	r.HandleFunc("/igcinfo/api/igc/{id}", handlerMetaTrack).Methods("GET")
+	r.HandleFunc("/igcinfo/api/igc/{id}/{field}", handlerSpecificTrack).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
